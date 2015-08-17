16fa92429a66ff2d1ec9e8443eaf242483783c5a
emrinalgr <emrinalgr@mycompany.com>
2015-08-17 09:36:51 +0000

    Updated CommonGeneric JPA Dao
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@3556 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/service/JPACommonGenericDAO.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/service/JPACommonGenericDAO.java
@@ -3,10 +3,7 @@ package eu.europa.ec.fisheries.uvms.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
@@ -20,44 +17,40 @@ import java.util.Set;
  * This class is responsible for all application level database interaction.
  * It provides unified apis for all basic CRUD operations like Create, Read, Update, Delete.
  */
-@TransactionAttribute(TransactionAttributeType.REQUIRED)
 public abstract class JPACommonGenericDAO<T> implements CommonGenericDAO<T> {
 
     private static final Logger LOG = LoggerFactory.getLogger(JPACommonGenericDAO.class);
 
-    @PersistenceContext(unitName = "UVMS")
-    EntityManager em;
-
     @Override
     public T createEntity(final T entity) {
         LOG.debug("Persisting entity : " + entity.getClass().getSimpleName());
-        em.persist(entity);
+        getEntityManager().persist(entity);
         return entity;
     }
 
     @Override
     public T updateEntity(final T entity) {
         LOG.debug("Updating entity : " + entity.getClass().getSimpleName());
-        em.merge(entity);
+        getEntityManager().merge(entity);
         return entity;
     }
 
     @Override
     public T findEntityById(final Class<T> entityClass, final Object id) {
         LOG.debug("Finding entity : " + entityClass.getSimpleName() + " with ID : " + id.toString());
-        return em.find(entityClass, id);
+        return getEntityManager().find(entityClass, id);
     }
 
     @Override
     public List<T> findEntityByNativeQuery(String nativeQuery) {
         LOG.debug("Finding entity for query : " + nativeQuery);
-        return em.createNativeQuery(nativeQuery).getResultList();
+        return getEntityManager().createNativeQuery(nativeQuery).getResultList();
     }
 
     @Override
     public List<T> findEntityByNativeQuery(String nativeQuery, Map<String, String> parameters) {
         LOG.debug("Finding entity for query : " + nativeQuery);
-        Query query = em.createNativeQuery(nativeQuery);
+        Query query = getEntityManager().createNativeQuery(nativeQuery);
         for (Map.Entry<String, String> entry : parameters.entrySet()) {
             query.setParameter(entry.getKey(), entry.getValue());
         }
@@ -67,7 +60,7 @@ public abstract class JPACommonGenericDAO<T> implements CommonGenericDAO<T> {
     @Override
     public List<T> findEntityByHqlQuery(final Class<T> entityClass, final String hqlQuery) {
         LOG.debug("Finding entity for query : " + hqlQuery);
-        return em.createQuery(hqlQuery, entityClass).getResultList();
+        return getEntityManager().createQuery(hqlQuery, entityClass).getResultList();
     }
 
     @Override
@@ -75,7 +68,7 @@ public abstract class JPACommonGenericDAO<T> implements CommonGenericDAO<T> {
     public List<T> findEntityByHqlQuery(final Class<T> entityClass, final String hqlQuery, final Map<Integer, String> parameters) {
         LOG.debug("Finding entity for query : " + hqlQuery);
         Set<Map.Entry<Integer, String>> rawParameters = parameters.entrySet();
-        Query query = em.createQuery(hqlQuery, entityClass);
+        Query query = getEntityManager().createQuery(hqlQuery, entityClass);
         for (Map.Entry<Integer, String> entry : rawParameters) {
             query.setParameter(entry.getKey(), entry.getValue());
         }
@@ -87,7 +80,7 @@ public abstract class JPACommonGenericDAO<T> implements CommonGenericDAO<T> {
     public List<T> findEntityByHqlQuery(final Class<T> entityClass, final String hqlQuery, final Map<Integer, String> parameters, final int maxResultLimit) {
         LOG.debug("Finding entity for query : " + hqlQuery);
         Set<Map.Entry<Integer, String>> rawParameters = parameters.entrySet();
-        Query query = em.createQuery(hqlQuery, entityClass);
+        Query query = getEntityManager().createQuery(hqlQuery, entityClass);
         for (Map.Entry<Integer, String> entry : rawParameters) {
             query.setParameter(entry.getKey(), entry.getValue());
         }
@@ -99,13 +92,13 @@ public abstract class JPACommonGenericDAO<T> implements CommonGenericDAO<T> {
 
     @Override
     public List<T> findEntityByNamedQuery(final Class<T> entityClass, final String queryName) {
-        TypedQuery<T> query = em.createNamedQuery(queryName, entityClass);
+        TypedQuery<T> query = getEntityManager().createNamedQuery(queryName, entityClass);
         return query.getResultList();
     }
 
     @Override
     public List<T> findEntityByNamedQuery(Class<T> entityClass, String queryName, Map<String, String> parameters) {
-        TypedQuery<T> query = em.createNamedQuery(queryName, entityClass);
+        TypedQuery<T> query = getEntityManager().createNamedQuery(queryName, entityClass);
         for (Map.Entry<String, String> entry : parameters.entrySet()) {
             query.setParameter(entry.getKey(), entry.getValue());
         }
@@ -114,7 +107,7 @@ public abstract class JPACommonGenericDAO<T> implements CommonGenericDAO<T> {
 
     @Override
     public List<T> findEntityByNamedQuery(Class<T> entityClass, String queryName, Map<String, String> parameters, int maxResultLimit) {
-        TypedQuery<T> query = em.createNamedQuery(queryName, entityClass);
+        TypedQuery<T> query = getEntityManager().createNamedQuery(queryName, entityClass);
         for (Map.Entry<String, String> entry : parameters.entrySet()) {
             query.setParameter(entry.getKey(), entry.getValue());
         }
@@ -127,13 +120,15 @@ public abstract class JPACommonGenericDAO<T> implements CommonGenericDAO<T> {
     @Override
     public List<T> findAllEntity(final Class<T> entityClass) {
         LOG.debug("Finding all entity list for : " + entityClass.getSimpleName());
-        return em.createQuery("from " + entityClass.getSimpleName(), entityClass).getResultList();
+        return getEntityManager().createQuery("from " + entityClass.getSimpleName(), entityClass).getResultList();
     }
 
     @Override
     public void deleteEntity(final T entity, final Object id) {
         LOG.debug("Deleting entity : " + entity.getClass().getSimpleName());
-        em.remove(em.contains(entity) ? entity : em.merge(entity));
+        getEntityManager().remove(getEntityManager().contains(entity) ? entity : getEntityManager().merge(entity));
     }
+
+    protected abstract EntityManager getEntityManager();
 
 }
