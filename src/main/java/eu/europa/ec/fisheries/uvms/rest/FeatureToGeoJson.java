ff82139b1b4faf63108140bffffa5679aafdce9a
emrinalgr <emrinalgr@mycompany.com>
2015-08-20 13:34:33 +0000

    Added feature
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@3695 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ src/main/java/eu/europa/ec/fisheries/uvms/rest/FeatureToGeoJson.java
@@ -0,0 +1,69 @@
+package eu.europa.ec.fisheries.uvms.rest;
+
+import com.vividsolutions.jts.geom.Geometry;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.geojson.geom.GeometryJSON;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.opengis.feature.Property;
+import org.opengis.feature.simple.SimpleFeature;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * //TODO create test
+ */
+public class FeatureToGeoJson {
+
+    public JSONObject convert(SimpleFeatureCollection featureCollection) {
+        return buildFeatureCollection(featureCollection);
+    }
+
+    @SuppressWarnings("unchecked")
+    private JSONObject buildFeatureCollection(SimpleFeatureCollection featureCollection) {
+        List<JSONObject> features = new LinkedList<>();
+        SimpleFeatureIterator simpleFeatureIterator = featureCollection.features();
+        while (simpleFeatureIterator.hasNext()) {
+            SimpleFeature simpleFeature = simpleFeatureIterator.next();
+            features.add(buildFeature(simpleFeature));
+        }
+
+        JSONObject obj = new JSONObject();
+        obj.put("type", "FeatureCollection");
+        obj.put("features", features);
+        return obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    private JSONObject buildFeature(SimpleFeature simpleFeature) {
+        JSONObject obj = new JSONObject();
+        obj.put("type", "Feature");
+        obj.put("geometry", buildGeometry((Geometry) simpleFeature.getDefaultGeometry()));
+        obj.put("properties", buildProperties(simpleFeature));
+        return obj;
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private JSONObject buildProperties(SimpleFeature simpleFeature) {
+        JSONObject obj = new JSONObject();
+        Collection<Property> properties = simpleFeature.getProperties();
+        for (Property property : properties) {
+            if (!property.getName().getLocalPart().equals("geometry")){
+                obj.put(property.getName().toString(), property.getValue() == null ? "" : property.getValue().toString());
+            }
+        }
+        return obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    private JSONObject buildGeometry(Geometry geometry) {
+        GeometryJSON gjson = new GeometryJSON();
+        Object obj = JSONValue.parse(gjson.toString(geometry));
+        JSONObject jsonObj = (JSONObject) obj;
+        return jsonObj;
+    }
+}
