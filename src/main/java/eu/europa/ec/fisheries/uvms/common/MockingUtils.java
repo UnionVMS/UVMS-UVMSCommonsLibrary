0d65ecd903f8f3bae4b04f9fa885ec0cb846e51c
emrinalgr <emrinalgr@mycompany.com>
2015-09-01 13:48:37 +0000

    Random odd numbers generator
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@4131 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/common/MockingUtils.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/common/MockingUtils.java
@@ -1,5 +1,7 @@
 package eu.europa.ec.fisheries.uvms.common;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.Random;
 
 public class MockingUtils {
@@ -19,4 +21,17 @@ public class MockingUtils {
 
         return rand.nextInt((max - min) + 1) + min;
     }
+
+    public static int randIntOdd(int min, int max) {
+
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+
+        return (rand.nextInt((max - min) + 1) + min)& ~1;
+    }
+
 }
