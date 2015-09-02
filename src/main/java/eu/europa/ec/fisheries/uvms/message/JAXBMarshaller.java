ea0c7ef5f1764c00e6533871f822f37b0267bf1a
emrinalgr <emrinalgr@mycompany.com>
2015-09-02 14:51:15 +0000

    JAXBMarshaller
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@4179 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ src/main/java/eu/europa/ec/fisheries/uvms/message/JAXBMarshaller.java
@@ -0,0 +1,56 @@
+package eu.europa.ec.fisheries.uvms.message;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JAXBMarshaller {
+
+    private static Logger LOG = LoggerFactory.getLogger(JAXBMarshaller.class);
+
+    /**
+     * Marshalls a JAXB Object to a XML String representation
+     *
+     * @param <T>
+     * @param data
+     * @return
+     * @throws
+     */
+    public static <T> String marshallJaxBObjectToString(T data) throws JAXBException {
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(data.getClass());
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        StringWriter sw = new StringWriter();
+        marshaller.marshal(data, sw);
+        return sw.toString();
+    }
+
+    /**
+     * Unmarshalls A textMessage to the desired Object. The object must be the
+     * root object of the unmarshalled message!
+     *
+     * @param <R>
+     * @param textMessage
+     * @param clazz pperException
+     * @return
+     * @throws
+     */
+    public static <R> R unmarshallTextMessage(final TextMessage textMessage, final Class clazz) throws JAXBException, JMSException {
+        JAXBContext jc = JAXBContext.newInstance(clazz);
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        StringReader sr = new StringReader(textMessage.getText());
+        return (R) unmarshaller.unmarshal(sr);
+    }
+
+}
+
