9584c4df5f0d4583e60adb13976b377fdebfd15f
empadhyad <empadhyad@mycompany.com>
2015-09-16 08:59:20 +0000

    [Commons]
    Added new error code
    
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@4537 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/rest/constants/ErrorCodes.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/rest/constants/ErrorCodes.java
@@ -3,4 +3,5 @@ package eu.europa.ec.fisheries.uvms.rest.constants;
 public interface ErrorCodes {
 	String DELETE_FAILED = "error_delete_failed";
 	String ENTRY_NOT_FOUND = "error_entry_not_found";
+	String INPUT_NOT_SUPPORTED = "INPUT_NOT_SUPPORTED";
 }
