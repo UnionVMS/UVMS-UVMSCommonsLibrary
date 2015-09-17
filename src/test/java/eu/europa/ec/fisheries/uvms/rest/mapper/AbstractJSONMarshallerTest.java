5f163807b7f35b8e99dc118e49b7ad4e048385f9
emrinalgr <emrinalgr@mycompany.com>
2015-09-17 08:55:59 +0000

    Added abstract json marshaller + test
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@4585 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ src/test/java/eu/europa/ec/fisheries/uvms/rest/mapper/AbstractJSONMarshallerTest.java
@@ -0,0 +1,69 @@
+package eu.europa.ec.fisheries.uvms.rest.mapper;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AbstractJSONMarshallerTest {
+
+    @Test
+    public void testMarshallJsonStringToObject() throws IOException {
+
+        JSONMarshaller jsonMarshaller = new JSONMarshaller();
+        URL url = Resources.getResource("testMarshallJsonStringToObject.json");
+        String json = Resources.toString(url, Charsets.UTF_8);
+        User user = jsonMarshaller.marshallJsonStringToObject(json, User.class);
+        assertEquals("greg", user.getName());
+        assertEquals(29, user.getAge());
+        assertEquals(3, user.getMessages().size());
+
+    }
+
+    private class JSONMarshaller extends AbstractJSONMarshaller {
+
+    }
+
+    private static class User {
+
+        private int age = 29;
+        private String name = "greg";
+        private List<String> messages = new ArrayList<String>() {
+            {
+                add("msg 1");
+                add("msg 2");
+                add("msg 3");
+            }
+        };
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public List<String> getMessages() {
+            return messages;
+        }
+
+        public void setMessages(List<String> messages) {
+            this.messages = messages;
+        }
+    }
+}
