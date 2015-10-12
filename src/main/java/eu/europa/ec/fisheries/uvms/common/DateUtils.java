21fdeb246daa99cd02953d5c7b80d7507098f0e7
emrinalgr <emrinalgr@mycompany.com>
2015-10-12 08:52:46 +0000

    Fix datetime stuff
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@5403 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/common/DateUtils.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/common/DateUtils.java
@@ -20,12 +20,10 @@ public class DateUtils {
     final static org.slf4j.Logger LOG = LoggerFactory.getLogger(DateUtils.class);
 
     public static final String DATE_TIME_UI_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
-    public static final String DATE_TIME_FILTER_FORMAT = "yyyy-MM-dd HH:mm:ss";
     final static String FORMAT = "yyyy-MM-dd HH:mm:ss Z";
 
     // thread safe formatter
     public static DateTimeFormatter UI_FORMATTER = DateTimeFormat.forPattern(DATE_TIME_UI_FORMAT);
-    public static DateTimeFormatter FILTER_FORMATTER = DateTimeFormat.forPattern(DATE_TIME_FILTER_FORMAT);
 
     private DateUtils() {
 
