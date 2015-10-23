d1013e9e4b0958c5d27c0c90741ce9d9f504db7f
Georgi Georgiev <georgi.georgiev@ordina.be>
2015-10-23 12:57:32 +0000

    Add USM queue to the constants
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@5906 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/message/MessageConstants.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/message/MessageConstants.java
@@ -1,26 +1,23 @@
 package eu.europa.ec.fisheries.uvms.message;
 
-public class MessageConstants {
+public interface MessageConstants {
 
-    private MessageConstants(){
+    String CONNECTION_FACTORY = "java:/ConnectionFactory";
+    String CONNECTION_TYPE = "javax.jms.MessageListener";
+    String DESTINATION_TYPE_QUEUE = "javax.jms.Queue";
 
-    }
-
-    public static final String CONNECTION_FACTORY = "java:/ConnectionFactory";
-    public static final String CONNECTION_TYPE = "javax.jms.MessageListener";
-    public static final String DESTINATION_TYPE_QUEUE = "javax.jms.Queue";
-
-    public static final String QUEUE_MODULE_SPATIAL = "java:/jms/queue/UVMSSpatialEvent";
-    public static final String QUEUE_MODULE_SPATIAL_NAME = "UVMSSpatialEvent";
-    public static final String COMPONENT_MESSAGE_IN_QUEUE = "UVMSMovementEvent";
-    public static final String COMPONENT_MESSAGE_IN_QUEUE_NAME = "UVMSMovementEvent";
-    public static final String COMPONENT_RESPONSE_QUEUE = "java:/jms/queue/UVMSMovement";
-    public static final String QUEUE_DATASOURCE_INTERNAL = "java:/jms/queue/UVMSMovementModel";
-    public static final String QUEUE_VESSEL = "java:/jms/queue/UVMSVessel"; //response queue
-    public static final String QUEUE_VESSEL_EVENT = "java:/jms/queue/UVMSVesselEvent"; //request queue, create event
-    public static final String QUEUE_MODULE_MOVEMENT = "java:/jms/queue/UVMSMovementEvent";
-    public static final String QUEUE_AUDIT_EVENT = "java:/jms/queue/UVMSAuditEvent";
-    public static final String QUEUE_AUDIT = "java:/jms/queue/UVMSAudit";
-    public static final String QUEUE_REPORTING = "java:/jms/queue/UVMSReporting";
+    String QUEUE_MODULE_SPATIAL = "java:/jms/queue/UVMSSpatialEvent";
+    String QUEUE_MODULE_SPATIAL_NAME = "UVMSSpatialEvent";
+    String COMPONENT_MESSAGE_IN_QUEUE = "UVMSMovementEvent";
+    String COMPONENT_MESSAGE_IN_QUEUE_NAME = "UVMSMovementEvent";
+    String COMPONENT_RESPONSE_QUEUE = "java:/jms/queue/UVMSMovement";
+    String QUEUE_DATASOURCE_INTERNAL = "java:/jms/queue/UVMSMovementModel";
+    String QUEUE_VESSEL = "java:/jms/queue/UVMSVessel"; //response queue
+    String QUEUE_VESSEL_EVENT = "java:/jms/queue/UVMSVesselEvent"; //request queue, create event
+    String QUEUE_MODULE_MOVEMENT = "java:/jms/queue/UVMSMovementEvent";
+    String QUEUE_AUDIT_EVENT = "java:/jms/queue/UVMSAuditEvent";
+    String QUEUE_AUDIT = "java:/jms/queue/UVMSAudit";
+    String QUEUE_REPORTING = "java:/jms/queue/UVMSReporting";
+    String QUEUE_USM = "java:/jms/queue/UVMSUserEvent";
 
 }
