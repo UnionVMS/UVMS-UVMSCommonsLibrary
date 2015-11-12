1c2a8070bacf6be8bc930f179112a72567e2e258
emrinalgr <emrinalgr@mycompany.com>
2015-11-12 08:38:42 +0000

    Fix warning
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@6655 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/message/AbstractJAXBMarshaller.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/message/AbstractJAXBMarshaller.java
@@ -24,7 +24,7 @@ public abstract class AbstractJAXBMarshaller {
      * @return
      * @throws
      */
-    protected static <T> String marshallJaxBObjectToString(final T data) throws JAXBException {
+    protected <T> String marshallJaxBObjectToString(final T data) throws JAXBException {
 
         JAXBContext jaxbContext = JAXBContext.newInstance(data.getClass());
         Marshaller marshaller = jaxbContext.createMarshaller();
@@ -44,7 +44,7 @@ public abstract class AbstractJAXBMarshaller {
      * @return
      * @throws
      */
-    protected static <R> R unmarshallTextMessage(final TextMessage textMessage, final Class clazz) throws JAXBException, JMSException {
+    protected <R> R unmarshallTextMessage(final TextMessage textMessage, final Class clazz) throws JAXBException, JMSException {
         JAXBContext jc = JAXBContext.newInstance(clazz);
         Unmarshaller unmarshaller = jc.createUnmarshaller();
         StringReader sr = new StringReader(textMessage.getText());
