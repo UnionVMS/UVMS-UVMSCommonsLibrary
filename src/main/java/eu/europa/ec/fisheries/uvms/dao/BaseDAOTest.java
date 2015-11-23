95e33a015c95f587d0103c6cc0b10b3100ea58c2
emrinalgr <emrinalgr@mycompany.com>
2015-11-23 07:29:10 +0000

    Introduce h2spatial-ext
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/uvms-commons@6861 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/dao/BaseDAOTest.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/dao/BaseDAOTest.java
@@ -1,14 +1,17 @@
 package eu.europa.ec.fisheries.uvms.dao;
 
 import com.ninja_squad.dbsetup.DbSetupTracker;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.h2.jdbcx.JdbcConnectionPool;
+import org.h2gis.h2spatialext.CreateSpatialExtension;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @Slf4j
 public abstract class BaseDAOTest {
@@ -17,7 +20,7 @@ public abstract class BaseDAOTest {
     private final String TEST_DB_PASSWORD = StringUtils.EMPTY;
     private final String TEST_DB_URL = "jdbc:h2:mem:testdb;INIT=CREATE SCHEMA IF NOT EXISTS "+ getSchema() +";DATABASE_TO_UPPER=false;TRACE_LEVEL_SYSTEM_OUT=2";
 
-    protected DbSetupTracker dbSetupTracker = new DbSetupTracker();
+    protected static DbSetupTracker dbSetupTracker = new DbSetupTracker();
 
     protected DataSource ds = JdbcConnectionPool.create(TEST_DB_URL, TEST_DB_USER, TEST_DB_PASSWORD);
 
@@ -25,7 +28,10 @@ public abstract class BaseDAOTest {
 
     protected EntityManager em;
 
+    @SneakyThrows
     public BaseDAOTest() {
+
+        CreateSpatialExtension.initSpatialExtension(ds.getConnection());
 
         log.info("BuildingEntityManager for unit tests");
         emFactory = Persistence.createEntityManagerFactory(getPersistenceUnitName());
