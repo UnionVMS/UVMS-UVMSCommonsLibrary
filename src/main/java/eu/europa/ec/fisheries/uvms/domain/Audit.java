1dd34656b3941ed86bdc0978d81afffdc07ea203
emrinalgr <emrinalgr@mycompany.com>
2016-04-26 19:12:42 +0000

    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Libraries/uvms-commons@11691 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ src/main/java/eu/europa/ec/fisheries/uvms/domain/Audit.java
@@ -0,0 +1,39 @@
+package eu.europa.ec.fisheries.uvms.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Embeddable
+@EqualsAndHashCode
+@ToString
+public class Audit implements Serializable {
+
+    public static final String CREATED_ON = "created_on";
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = CREATED_ON, nullable = false)
+    private Date createdOn;
+
+    Audit() {
+    }
+
+    @Builder
+    public Audit(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+}
