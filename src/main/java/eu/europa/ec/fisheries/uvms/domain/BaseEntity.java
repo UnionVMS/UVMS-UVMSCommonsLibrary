f02449cbb00194c80862b8e162834f4eace70ccc
emrinalgr <emrinalgr@mycompany.com>
2016-05-25 14:51:00 +0000

    Add sequences in comment
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Libraries/uvms-commons@12437 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/domain/BaseEntity.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/domain/BaseEntity.java
@@ -16,6 +16,7 @@ public class BaseEntity implements Serializable {
 
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "default_gen")
     private final Long id;
 
     protected BaseEntity(){
