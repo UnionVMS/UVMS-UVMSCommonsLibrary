1a7b9557343dc4de627b125c4f7bc4cd93d2d617
emrinalgr <emrinalgr@mycompany.com>
2016-05-25 19:39:36 +0000

    Add sequences in comment, extend from baseentity
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Libraries/uvms-commons@12442 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/domain/BaseEntity.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/domain/BaseEntity.java
@@ -15,7 +15,7 @@ import lombok.ToString;
 public class BaseEntity implements Serializable {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "default_gen")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private final Long id;
 
     protected BaseEntity(){
