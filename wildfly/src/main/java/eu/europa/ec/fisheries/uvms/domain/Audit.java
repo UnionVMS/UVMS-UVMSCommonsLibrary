4a1376f1d3e4b1c06cbddae47f6a16d7b17ccfee
AdityaPadhy <aditya.padhy123@gmail.com>
2017-01-18 17:40:26 +0100

    created two modules weblogic and wildfly

---
+++ wildfly/src/main/java/eu/europa/ec/fisheries/uvms/domain/Audit.java
@@ -0,0 +1,51 @@
+/*
+Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries @ European Union, 2015-2016.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of
+the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+
+package eu.europa.ec.fisheries.uvms.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+@SuppressWarnings("serial")
+@Embeddable
+@EqualsAndHashCode
+@ToString
+public class Audit implements Serializable {
+
+    public static final String CREATED_ON = "created_on";
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = CREATED_ON, nullable = false)
+    private Date createdOn;
+
+    Audit() {
+    }
+
+    public Audit(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+}
