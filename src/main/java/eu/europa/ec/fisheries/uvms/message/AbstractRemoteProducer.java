3611acf73666aa1d4fd9d1d441ca4fb3294ae1b2
emsanerac <Rachana.Sane@arhs-developments.com>
2017-04-14 14:42:01 +0200

    UNIONVMS-1913 : copy right added

--- src/main/java/eu/europa/ec/fisheries/uvms/message/AbstractRemoteProducer.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/message/AbstractRemoteProducer.java
@@ -1,3 +1,14 @@
+/*
+ ﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+ © European Union, 2015-2016.
+
+ This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+ redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+ the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+ copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
 package eu.europa.ec.fisheries.uvms.message;
 
 import eu.europa.ec.fisheries.uvms.exception.ServiceException;
@@ -169,17 +180,13 @@ public abstract class AbstractRemoteProducer implements MessageProducer {
         try {
             LOG.debug("Opening connection to JMS broker:"+connectionProperties);
             connection = connectionFactory.createConnection(connectionProperties.getUsername(), connectionProperties.getPassword());
-            LOG.debug("Connection created");
             connection.start();
-            LOG.debug("Connection started");
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             LOG.debug("session created");
         } catch (JMSException e) {
             LOG.error("Error when open connection to JMS broker. Going to << RETRY >> now.", e);
-            throw e;
-          //  retryConnecting();
+            retryConnecting();
         }
-
 
     }
 
