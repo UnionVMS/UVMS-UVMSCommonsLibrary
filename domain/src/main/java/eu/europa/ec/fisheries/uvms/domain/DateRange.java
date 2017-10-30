9a8820ac3972feb3f4aea45393077b2b0a56c2b5
James Sörling <james.p.sorling@consid.se>
2017-10-30 13:16:08 +0100

    breakdown modules, wip

---
+++ domain/src/main/java/eu/europa/ec/fisheries/uvms/domain/DateRange.java
@@ -0,0 +1,93 @@
+/*
+﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+© European Union, 2015-2016.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.europa.ec.fisheries.uvms.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import eu.europa.ec.fisheries.uvms.serializer.DateUiFormatSerializer;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.search.annotations.DateBridge;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.Resolution;
+
+@Embeddable
+@EqualsAndHashCode
+@ToString
+@Indexed
+public class DateRange implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	public static final String END_DATE = "end_date";
+    public static final String START_DATE = "start_date";
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = START_DATE)
+    @JsonSerialize(using = DateUiFormatSerializer.class)
+    @Field(name = "startDate")
+    @DateBridge(resolution= Resolution.SECOND)
+    private Date startDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = END_DATE)
+    @Field(name = "endDate")
+    @JsonSerialize(using = DateUiFormatSerializer.class)
+    @DateBridge(resolution= Resolution.SECOND)
+    private Date endDate;
+
+    public DateRange() {
+    }
+
+    public DateRange(Date startDate, Date endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Date getStartDate() {
+        Date startDate = null;
+        if (this.startDate != null) {
+            startDate = new Date(this.startDate.getTime());
+        }
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        if (startDate != null) {
+            this.startDate = new Date(startDate.getTime());
+        }
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        Date endDate = null;
+        if (this.endDate != null) {
+            endDate = new Date(this.endDate.getTime());
+        }
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        if (endDate != null) {
+            this.endDate = new Date(endDate.getTime());
+        }
+        this.endDate = endDate;
+    }
+
+
+}
